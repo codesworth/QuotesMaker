c737188424935644bc6461a7acc863433bb1de42
Codesworth <codezworth@gmail.com>
2019-02-24 22:31:58 +0000

    Initial Commit

---
+++ QuotesMakerTests/QuotesMakerTests.swift
@@ -0,0 +1,34 @@
+//
+//  QuotesMakerTests.swift
+//  QuotesMakerTests
+//
+//  Created by Shadrach Mensah on 24/02/2019.
+//  Copyright © 2019 Shadrach Mensah. All rights reserved.
+//
+
+import XCTest
+@testable import QuotesMaker
+
+class QuotesMakerTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
