9f0e837efc97e298a28776c2675d73ad28a884ac
Codesworth <codezworth@gmail.com>
2019-02-27 10:02:46 +0000

    Created Images Layer Tests

--- QuotesMaker/ImagesVC.swift
+++ QuotesMaker/ImagesVC.swift
@@ -12,12 +12,21 @@ class ImagesVC: UIViewController {
     
 
     @IBOutlet weak var baseView: UIView!
+    var imageLayer:CALayer!
+    var textlayer:CATextLayer!
+    var filterLayer:CALayer!
+    var operationIndex = 0
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
+    @objc func imageTapped(){
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        present(picker, animated: true, completion: nil)
+    }
 
     /*
     // MARK: - Navigation
@@ -29,4 +38,47 @@ class ImagesVC: UIViewController {
     }
     */
 
+    @IBAction func addImageButt(_ sender: Any) {
+        switch operationIndex {
+        case 0:
+            imageTapped()
+            break
+        case 1:
+            filterLayer = CALayer()
+            filterLayer.bounds = baseView.bounds
+            baseView.layer.addSublayer(filterLayer)
+            filterLayer.backgroundColor = #colorLiteral(red: 0.2588235438, green: 0.7568627596, blue: 0.9686274529, alpha: 0.4).cgColor
+            break
+        case 2:
+            textlayer = CATextLayer()
+            textlayer.bounds = baseView.bounds
+            textlayer.alignmentMode = .center
+            textlayer.string = NSAttributedString(string: "Hello Warpi Image", attributes: [.font : UIFont.systemFont(ofSize: 20, weight: .bold),.foregroundColor:UIColor.red.cgColor])
+            baseView.layer.addSublayer(textlayer)
+            break
+        default:
+            break
+        }
+         operationIndex += 1
+    }
+}
+
+
+
+extension ImagesVC:UIImagePickerControllerDelegate,UINavigationControllerDelegate{
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let image = info[.originalImage] as? UIImage{
+            imageLayer = CALayer()
+            imageLayer.frame = baseView.bounds
+            baseView.layer.addSublayer(imageLayer)
+            imageLayer.contents = image.cgImage
+            imageLayer.masksToBounds = true
+        }
+        picker.dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true, completion: nil)
+    }
 }
