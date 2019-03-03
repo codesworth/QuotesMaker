76a313d50bb247a2039df907b294026e3c0d7c8a
Codesworth <codezworth@gmail.com>
2019-03-03 06:39:36 +0000

    Completed Gradient View

--- QuotesMaker/Views/GradientOptionsView.swift
+++ QuotesMaker/Views/GradientOptionsView.swift
@@ -16,6 +16,9 @@ class GradientOptionsView: UIView {
         return lable
     }()
     
+    private let insets:CGFloat = 12
+    private let margin:CGFloat = 8
+    
     lazy var stepperTitle:BasicLabel = {
         let lable = BasicLabel(frame: .zero)
         lable.text = "Add / Remove Colors"
@@ -42,7 +45,7 @@ class GradientOptionsView: UIView {
         return slider
     }()
     
-    lazy var locationSLider:UISlider = {
+    lazy var locationSlider:UISlider = {
        let slider  = UISlider(frame: .zero)
         slider.minimumValue = 0
         slider.maximumValue = 1
@@ -62,6 +65,11 @@ class GradientOptionsView: UIView {
         
     }
     
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        commonInit()
+    }
+    
     func commonInit(){
         addSubview(titleLable)
         addSubview(gradientSegments)
@@ -69,7 +77,28 @@ class GradientOptionsView: UIView {
         addSubview(stepper)
         addSubview(colorSlider)
         addSubview(locationTitle)
-        addSubview(locationSLider)
+        addSubview(locationSlider)
+        gradientSegments.addTarget(self, action: #selector(gradSegmentChanged(_:)), for: .valueChanged)
+        colorSlider.addTarget(self, action: #selector(colorSliderChanged(_:)), for: .valueChanged)
+        stepper.addTarget(self, action: #selector(stepperChanged(_:)), for: .valueChanged)
+        locationSlider.addTarget(self, action: #selector(locationSliderChanged(_:)), for: .valueChanged)
+        
+    }
+    
+    @objc func gradSegmentChanged(_ sender:UISegmentedControl){
+        
+    }
+    
+    @objc func colorSliderChanged(_ slider:ColorSlider){
+        
+    }
+    
+    @objc func stepperChanged(_ sender:UIStepper){
+        
+    }
+    
+    @objc func locationSliderChanged(_ slider:UISlider){
+        
     }
     
     required init?(coder aDecoder: NSCoder) {
@@ -80,7 +109,28 @@ class GradientOptionsView: UIView {
         super.layoutSubviews()
         subviews.forEach{$0.translatesAutoresizingMaskIntoConstraints = false}
         NSLayoutConstraint.activate([
-            
+            titleLable.topAnchor.constraint(equalTo: topAnchor, constant: insets),
+            titleLable.centerXAnchor.constraint(equalTo: centerXAnchor),
+            gradientSegments.topAnchor.constraint(equalTo: titleLable.bottomAnchor, constant: margin),
+            gradientSegments.leadingAnchor.constraint(equalTo: leadingAnchor, constant: insets),
+            gradientSegments.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -insets),
+            gradientSegments.heightAnchor.constraint(equalToConstant: 28),
+            stepperTitle.topAnchor.constraint(equalTo: gradientSegments.bottomAnchor, constant: margin),
+            stepperTitle.leadingAnchor.constraint(equalTo: leadingAnchor, constant: insets),
+            stepper.topAnchor.constraint(equalTo: gradientSegments.bottomAnchor, constant: margin),
+            stepper.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -insets),
+            stepper.heightAnchor.constraint(equalToConstant: 25),
+            stepper.widthAnchor.constraint(equalToConstant: 60),
+            colorSlider.topAnchor.constraint(equalTo: stepperTitle.bottomAnchor, constant: margin),
+            colorSlider.leadingAnchor.constraint(equalTo: leadingAnchor, constant: insets),
+            colorSlider.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -insets),
+            colorSlider.heightAnchor.constraint(equalToConstant: 25),
+            locationTitle.topAnchor.constraint(equalTo: colorSlider.bottomAnchor, constant: margin),
+            locationTitle.leadingAnchor.constraint(equalTo: leadingAnchor, constant: insets),
+            locationSlider.topAnchor.constraint(equalTo: locationTitle.bottomAnchor, constant: margin),
+            locationSlider.leadingAnchor.constraint(equalTo: leadingAnchor, constant: insets),
+            locationSlider.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -insets),
+            locationSlider.heightAnchor.constraint(equalToConstant: 20),
         ])
     }
 
