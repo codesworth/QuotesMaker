e97536bf3e7b85ff75ee8785fde0ed8f599a5970
Codesworth <codezworth@gmail.com>
2019-03-18 15:00:58 +0000

    Bug Fixes on Image Rotation

--- QuotesMaker/Core/BackingImageView.swift
+++ QuotesMaker/Core/BackingImageView.swift
@@ -119,7 +119,11 @@ extension BackingImageView{
     
     func rotateImage(){
         guard let image = image else {return}
+//         currentAngle = currentAngle < 361 ? currentAngle + 90 : 0
+//        transform = transform.rotated(by: .Angle(90))
+        
         if let newImage = rotate(image){
+            //image.rotate(radians: Float(CGFloat.Angle(90))){
             self.image = newImage
         }
         
@@ -141,19 +145,46 @@ extension BackingImageView{
     }
     
     private func rotate(_ image:UIImage) -> UIImage?{
-        let angle = currentAngle < 361 ? currentAngle + 90 : 0
-        let rotatedSize = CGRect(origin: .zero, size: image.size).applying(CGAffineTransform(rotationAngle: .Angle(angle))).integral.size
-        UIGraphicsBeginImageContext(rotatedSize)
+        
+        var rotatedSize = CGRect(origin: .zero, size: image.size).applying(CGAffineTransform(rotationAngle: .Angle(90))).size
+        rotatedSize.width = floor(rotatedSize.width)
+        rotatedSize.height = floor(rotatedSize.height)
+        UIGraphicsBeginImageContextWithOptions(rotatedSize, false, image.scale)
         guard let context = UIGraphicsGetCurrentContext() else {return nil}
         let origin = CGPoint(x: rotatedSize.width / 2, y: rotatedSize.height / 2)
         context.translateBy(x: origin.x, y: origin.y)
-        context.rotate(by: .Angle(angle))
-        image.draw(in: [-origin.x, origin.y,image.size.width,image.size.height])
+        context.rotate(by: .Angle(90))
+        image.draw(in: [-image.size.width / 2, -image.size.height / 2,image.size.width,image.size.height])
         let rotatedImage = UIGraphicsGetImageFromCurrentImageContext()
         UIGraphicsEndImageContext()
         
         return rotatedImage
         
         
+    }
+}
+
+
+extension UIImage {
+    func rotate(radians: Float) -> UIImage? {
+        var newSize = CGRect(origin: CGPoint.zero, size: self.size).applying(CGAffineTransform(rotationAngle: CGFloat(radians))).size
+        // Trim off the extremely small float value to prevent core graphics from rounding it up
+        newSize.width = floor(newSize.width)
+        newSize.height = floor(newSize.height)
+        
+        UIGraphicsBeginImageContextWithOptions(newSize, false, self.scale)
+        let context = UIGraphicsGetCurrentContext()!
+        
+        // Move origin to middle
+        context.translateBy(x: newSize.width/2, y: newSize.height/2)
+        // Rotate around middle
+        context.rotate(by: CGFloat(radians))
+        // Draw the image at its center
+        self.draw(in: CGRect(x: -self.size.width/2, y: -self.size.height/2, width: self.size.width, height: self.size.height))
+        
+        let newImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        return newImage
     }
 }
