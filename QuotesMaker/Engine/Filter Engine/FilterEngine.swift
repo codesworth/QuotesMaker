cfa56f73426472c39d2aa5d7db5d1c5e0e123eb8
Codesworth <codezworth@gmail.com>
2019-05-15 23:02:53 +0000

    Added Custom Filter library

---
+++ QuotesMaker/Engine/Filter Engine/FilterEngine.swift
@@ -0,0 +1,14 @@
+//
+//  FilterEngine.swift
+//  QuotesMaker
+//
+//  Created by Shadrach Mensah on 15/05/2019.
+//  Copyright © 2019 Shadrach Mensah. All rights reserved.
+//
+
+import UIKit
+
+class FilterEngine:NSObject{
+    
+    
+}
