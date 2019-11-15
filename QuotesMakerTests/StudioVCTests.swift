7592bc42512d52a10b38adbc2af8863f9a7e5a42
Codesworth <codezworth@gmail.com>
2019-11-15 20:01:35 +0000

    Tests for iPhone StudioVC

--- QuotesMakerTests/StudioVCTests.swift
+++ QuotesMakerTests/StudioVCTests.swift
@@ -23,7 +23,7 @@ class StudioVCTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func test_tapping_AddLayerOptionAddsALayerToBaseView(){
+    func test_tapping_AddLayerOptionAddsABlankLayerToBaseView(){
         studioVc.loadViewIfNeeded()
         
         XCTAssertNil(studioVc.coordinator.baseView.currentSubview,"Error Initialized with a default subview")
@@ -33,6 +33,48 @@ class StudioVCTests: XCTestCase {
         
 
     }
+    
+    func test_tapping_AddImageOptionAddsImageLayerToBaseView(){
+        studioVc.loadViewIfNeeded()
+        
+        XCTAssertNil(studioVc.coordinator.baseView.currentSubview,"Error Initialized with a default subview")
+        studioVc.studioPanel.collectionView(studioVc.studioPanel.collectionView, didSelectItemAt: IndexPath(row: 2, section: 0))
+        XCTAssertNotNil(studioVc.coordinator.baseView.currentSubview,"Current Baseview has no current subview")
+        XCTAssertNotNil(studioVc.coordinator.baseView.currentSubview as? BackingImageView, "Wrong Layer Added")
+        
+
+    }
+    
+    func test_tapping_AddTextOptionAddsTextLayerToBaseView(){
+        studioVc.loadViewIfNeeded()
+        
+        XCTAssertNil(studioVc.coordinator.baseView.currentSubview,"Error Initialized with a default subview")
+        studioVc.studioPanel.collectionView(studioVc.studioPanel.collectionView, didSelectItemAt: IndexPath(row: 3, section: 0))
+        XCTAssertNotNil(studioVc.coordinator.baseView.currentSubview,"Current Baseview has no current subview")
+        XCTAssertNotNil(studioVc.coordinator.baseView.currentSubview as? BackingTextView, "Wrong Layer Added")
+        
+
+    }
+    
+    func test_tapping_StartOverOptionClearsBaseViewSubviews(){
+        studioVc.loadViewIfNeeded()
+        
+        XCTAssertNil(studioVc.coordinator.baseView.currentSubview,"Error Initialized with a default subview")
+        for _ in 0...5{
+            studioVc.studioPanel.collectionView(studioVc.studioPanel.collectionView, didSelectItemAt: IndexPath(row: 1, section: 0))
+        }
+        
+        XCTAssertNotNil(studioVc.coordinator.baseView.currentSubview,"Current Baseview has no current subview")
+        XCTAssertEqual(studioVc.coordinator.baseView.subviews.count, 6)
+        studioVc.studioPanel.collectionView(studioVc.studioPanel.collectionView, didSelectItemAt: IndexPath(row: 6, section: 0))
+        
+        XCTAssertNil(studioVc.coordinator.baseView.currentSubview, "Failed to clear CUrrebtSubview")
+        XCTAssertEqual(studioVc.coordinator.baseView.subviews.count, 0)
+        
+
+    }
+    
+    
 
     func testPerformanceExample() {
         // This is an example of a performance test case.
